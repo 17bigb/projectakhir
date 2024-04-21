608449409c44693b8067bb57fc7e723fb4458db6
17bigb <erwinmarbinototamba@gmail.com>
2024-04-21 23:51:57 +0700

    done Admin Auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admin\HomeController;
 use App\Http\Controllers\admin\AdminLoginController;
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin/login',[AdminLoginController::class,'index'])->name('admin.login');
+Route::group(['prefix' => 'admin'], function(){
+    Route::group(['middlewate' => 'admin.guest'], function(){
+        Route::get('/login',[AdminLoginController::class,'index'])->name('admin.login');
+        Route::post('/authenticate',[AdminLoginController::class,'authenticate'])->name('admin.authenticate');
+    });
+
+    Route::group(['middlewate' => 'admin.auth'], function(){
+        Route::get('/dashboard',[HomeController::class,'index'])->name('admin.dashboard');
+        Route::get('/logout',[HomeController::class,'logout'])->name('admin.logout');
+    });
+});
